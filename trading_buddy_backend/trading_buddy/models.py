a357c131b4473d16982cd34338d16cf315c2e159
Aleksandr Shestakov <103767386+avtorgenii@users.noreply.github.com>
2025-05-15 22:16:21 +0200

    feat: database created

--- trading_buddy_backend/trading_buddy/models.py
+++ trading_buddy_backend/trading_buddy/models.py
@@ -6,25 +6,27 @@ from django.db.models import ForeignKey
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     deposit = models.DecimalField(decimal_places=2, default=0.00, max_digits=20)
-    available_margin = models.DecimalField(decimal_places=2, default=0.00, max_digits=20)
-    risk = models.DecimalField(decimal_places=5, default=0.00, max_digits=20)
+    risk_percent = models.DecimalField(decimal_places=5, default=0.00, max_digits=10)
 
+# Exchange account
 class Account(models.Model):
     name = models.CharField(max_length=120)
     api_key = models.TextField()
     secret_key = models.TextField()
     user = ForeignKey('User', related_name='accounts', on_delete=models.CASCADE)
 
-class Position(models.Model):
-    tool = models.CharField(max_length=100)
-
-    LONG = 'long'
-    SHORT = 'short'
-    SIDE_CHOICES = [
-        (LONG, 'Long'),
-        (SHORT, 'Short'),
+class Tool(models.Model):
+    name = models.CharField(max_length=120)
+    MARKET_CHOICES = [
+        ('crypto', 'Crypto'),
+        ('fund', 'Fund'),
+        ('forex', 'Forex'),
     ]
-    side = models.CharField(max_length=5, choices=SIDE_CHOICES)
+    market = models.CharField(max_length=50, choices=MARKET_CHOICES)
+
+# Position is temporary, all needed data from it will be transferred to connected Trade and then Position record will be removed
+class Position(models.Model):
+    tool = models.ForeignKey(Tool, on_delete=models.DO_NOTHING, related_name='positions')
 
     leverage = models.IntegerField()
 
@@ -48,7 +50,34 @@ class Position(models.Model):
     pnl = models.DecimalField(decimal_places=8, max_digits=20)
     commission = models.DecimalField(decimal_places=8, max_digits=20)
 
+    account = models.ForeignKey('Account', related_name='positions', on_delete=models.CASCADE)
+    trade = models.OneToOneField('Trade',related_name='positions' , on_delete=models.DO_NOTHING)
+
+
+class Trade(models.Model):
+    SIDE_CHOICES = [
+        ('long', 'Long'), # actual value in DB and human-readable name
+        ('short', 'Short'),
+    ]
+    side = models.CharField(max_length=5, choices=SIDE_CHOICES)
+
+    tool = models.ForeignKey(Tool, on_delete=models.DO_NOTHING, related_name='trades')
+
     start_time = models.DateTimeField()
     end_time = models.DateTimeField(null=True, blank=True)
 
-    account = models.ForeignKey('Account', related_name='positions', on_delete=models.CASCADE)
+    MARKET_CHOICES = [
+        ('crypto', 'Crypto'),
+        ('fund', 'Fund'),
+        ('forex', 'Forex'),
+    ]
+    market = models.CharField(max_length=50, choices=MARKET_CHOICES)
+
+    risk_percent = models.DecimalField(decimal_places=2, max_digits=10)
+    risk_usd = models.DecimalField(decimal_places=2, max_digits=10)
+    pnl_usd = models.DecimalField(decimal_places=2, max_digits=10, help_text="Net profit, after commissions")
+    commission_usd = models.DecimalField(decimal_places=2, max_digits=10)
+
+    description = models.TextField()
+    result = models.TextField()
+    screenshot = models.ImageField(upload_to='screenshots')
