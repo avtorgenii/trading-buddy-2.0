fbea3c0f72a687a81bcd7b00b1b15ac0960ed74e
Tomek4861 <Tomek4861g@gmail.com>
2025-06-01 01:30:50 +0200

    feat: add chart wideget component

---
+++ trading_buddy_fe/src/lib/TradingViewWidget.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from "svelte";
+	export let symbol = "BYBIT:BTCUSDT.P";
+	let chartDiv;
+
+	onMount(() => {
+		chartDiv.innerHTML = "";
+
+		const script = document.createElement("script");
+		script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+		script.async = true;
+		script.innerHTML = JSON.stringify({
+			autosize: true,
+			symbol,
+			interval: "60",
+			timezone: "Europe/Warsaw",
+			theme: "dark",
+			style: "1",
+			locale: "en",
+			allow_symbol_change: false,
+			support_host: "https://www.tradingview.com"
+		});
+
+		chartDiv.appendChild(script);
+	});
+</script>
+
+<!--Needs to be wrapped with fixed width/height container-->
+<div class="tradingview-widget-container" bind:this={chartDiv} ></div>
---
+++ trading_buddy_fe/src/routes/trades/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import TradingViewWidget from '$lib/TradingViewWidget.svelte';
+
+</script>
+
+
+<div class="w-[500px] h-[2000px]">
+	<div class="h-[500px] w-[500px]">
+	<TradingViewWidget symbol="BYBIT:BTCUSDT.P" />
+	</div>
+	<div class="h-[500px] w-[500px]">
+
+	<TradingViewWidget symbol="BYBIT:ETHUSDT.P" />
+	</div>
+	<div class="h-[500px] w-[500px]">
+
+	<TradingViewWidget symbol="BINANCE:SOLUSDT" />
+	</div>
+
+</div>
+
+
