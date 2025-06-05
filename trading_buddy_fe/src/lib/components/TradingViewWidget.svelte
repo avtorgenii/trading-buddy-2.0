15738ac2655a796f6437aa2e034dbb02761ef744
Tomek4861 <Tomek4861g@gmail.com>
2025-06-05 15:32:28 +0200

    feat(positions): add mobile proper display & minor improvements

--- trading_buddy_fe/src/lib/components/TradingViewWidget.svelte
+++ trading_buddy_fe/src/lib/components/TradingViewWidget.svelte
@@ -2,22 +2,26 @@
 	import { onMount } from "svelte";
 	export let symbol = "BYBIT:BTCUSDT.P";
 	let chartDiv;
+	let isMobile = false;
 
 	onMount(() => {
 		chartDiv.innerHTML = "";
-
+		isMobile = window.innerWidth < 768;
 		const script = document.createElement("script");
 		script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
 		script.async = true;
 		script.innerHTML = JSON.stringify({
 			symbol,
 			autosize: true,
-			interval: "60",
+			interval: "30",
 			timezone: "Europe/Warsaw",
 			theme: "dark",
 			style: "1",
 			locale: "en",
 			allow_symbol_change: false,
+			save_image: false,
+			hide_top_toolbar: isMobile,
+			hide_volume: isMobile,
 			support_host: "https://www.tradingview.com"
 		});
 
--- trading_buddy_fe/src/routes/trades/+page.svelte
+++ trading_buddy_fe/src/routes/trades/+page.svelte
@@ -1,9 +1,29 @@
 <script>
 	import NavMenu from '$lib/components/NavMenu.svelte';
 	import TradingViewWidget from '$lib/components/TradingViewWidget.svelte';
+	import { onMount } from 'svelte';
+	import PositionCard from '$lib/components/PositionCard.svelte';
 
+
+	let positions = [];
 	let isCurrentSelected = true;
 	let container;
+
+	let screenWidth = 0;
+	$: isMobile = screenWidth < 768;
+
+	onMount(() => {
+
+		screenWidth = window.innerWidth;
+
+		window.addEventListener('resize', () => {
+			screenWidth = window.innerWidth;
+		});
+		positions = getCurrentPositions();setInterval(() => {
+				positions = getCurrentPositions();
+			}, 5000);
+		}
+	)
 
 	function getCurrentPositions() {
 		const generateRandomPnl = () => {
@@ -13,10 +33,13 @@
 		return [
 			{
 				positionId: '12345',
+				margin: 123.45,
+				value: 123.45,
 				openDate: new Date('2024-05-01T10:00:00Z'),
+				realizedPnl: generateRandomPnl(),
 				currentPnl: generateRandomPnl(),
-				currentPnlPercent: '50%',
-				ticker: 'BYBIT:BTCUSDT',
+				currentPnlPercent: 78.5,
+				ticker: 'BYBIT:BTCUSDT.P',
 				entryPrice: 170.25,
 				quantity: 10,
 				side: 'long',
@@ -26,9 +49,13 @@
 			},
 			{
 				positionId: '67890',
+				margin: 456.78,
+				value: 456.78,
 				openDate: new Date('2024-05-10T14:30:00Z'),
+				realizedPnl: generateRandomPnl(),
 				currentPnl: generateRandomPnl(),
-				ticker: 'BYBIT:ETHUSDT',
+				currentPnlPercent: 30.2,
+				ticker: 'BYBIT:ETHUSDT.P',
 				entryPrice: 180.50,
 				quantity: 5,
 				side: 'short',
@@ -37,9 +64,13 @@
 			},
 			{
 				positionId: '11223',
+				margin: 789.01,
+				value: 789.01,
 				openDate: new Date('2024-04-20T09:15:00Z'),
+				realizedPnl: generateRandomPnl(),
 				currentPnl: generateRandomPnl(),
-				ticker: 'BYBIT:SOLUSDT',
+				currentPnlPercent: 45.0,
+				ticker: 'BYBIT:SOLUSDT.P',
 				entryPrice: 400.00,
 				quantity: 7,
 				side: 'long',
@@ -50,27 +81,22 @@
 	}
 
 	function handleWheel(e) {
+		if (isMobile) return;
 		if (container.scrollWidth > container.clientWidth) {
 			e.preventDefault();
 			container.scrollLeft += e.deltaY;
 		}
 	}
-	function getStringAfterColon(inputString) {
-	  const parts = inputString.split(':');
-	  if (parts.length > 1) {
-	    return parts.slice(1).join(':');
-	  }
-	  return inputString;
-	}
+
 </script>
 
 
 <NavMenu />
 <div class="flex items-center flex-col">
 	<div
-		class="w-auto md:min-w-4/5 bg-zinc-900 px-2 md:px-10 pt-4 pb-12 rounded-2xl text-center flex flex-col justify-between min-h-[520px] max-w-xs md:max-w-lg shadow-xl shadow-white/10">
+		class="w-auto md:min-w-4/5 bg-zinc-900 md:px-10 pt-4 pb-12 rounded-2xl text-center flex flex-col justify-between min-h-[60vh] max-w-full md:max-w-lg shadow-xl shadow-white/10">
 		<div class="w-full flex justify-center">
-			<div class="flex w-100 max-w-md rounded-full border-2 border-zinc-700 bg-zinc-900">
+			<div class="flex w-100 max-w-md rounded-full border-2 border-zinc-700 bg-zinc-900 mx-5 md:mx-1">
 				<button class="flex-1 flex items-center justify-center py-2 text-white cursor-pointer rounded-l-full"
 								class:bg-zinc-800={isCurrentSelected}
 								on:click={() => isCurrentSelected = true}
@@ -91,39 +117,12 @@
 		{#if isCurrentSelected}
 			<div
 				bind:this={container}
-				class="mt-4 flex flex-nowrap overflow-x-auto -mx-2"
+				class="mt-4 flex {isMobile ? 'grid gap-24 grid-col-1' : 'flex-nowrap overflow-x-auto md:mx-2 scrollbar-win11'}"
 				on:wheel={handleWheel}
 				>
-				{#each getCurrentPositions() as position (position.positionId)}
+				{#each positions as position (position.positionId)}
+					<PositionCard {position} />
 
-					<div class="flex-none w-1/2 px-2">
-						<div class="bg-zinc-900 rounded-2xl shadow-xl shadow-white/10 h-full flex flex-col">
-							<!-- Chart -->
-							<div class="h-full bg-zinc-800 rounded-t-2xl flex items-center justify-center p-1">
-								<div class="h-[500px] w-[500px] block">
-								<TradingViewWidget symbol={position.ticker}/>
-								</div>
-							</div>
-							<!-- Data -->
-							<div class="p-4 flex-grow flex flex-col justify-between">
-								<div class="text-center">
-									<p class=" font-medium">{getStringAfterColon(position.ticker)}</p>
-									<p class="text-sm text-zinc-400">
-										Open Date: {position.openDate.toLocaleDateString()}
-									</p>
-								</div>
-								<div class="mt-4 text-center">
-									<p
-										class="text-lg font-semibold"
-										class:!text-red-400={position.currentPnl < 0}
-										class:!text-green-400={position.currentPnl >= 0}
-									>
-										PnL: {position.currentPnl.toFixed(2)}
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
 				{:else}
 					<div class="w-full px-2">
 						<p class="text-center text-zinc-400">No current positions.</p>
@@ -131,5 +130,38 @@
 				{/each}
 			</div>
 		{/if}
+		<button class=" mt-5 bg-blue-800 hover:bg-blue-700 py-3 rounded-xl w-full text-lg transition-colors duration-200 max-w-xs mx-auto">Open a Trade</button>
+
 	</div>
 </div>
+
+<style>
+
+    .scrollbar-win11::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+    }
+    .scrollbar-win11::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    .scrollbar-win11::-webkit-scrollbar-thumb {
+        background-color: rgba(255,255,255,0.2);
+        border-radius: 4px;
+        border: 2px solid transparent;
+    }
+
+    .scrollbar-win11::-webkit-scrollbar-thumb:hover {
+        background-color: rgba(255,255,255,0.4);
+    }
+
+    .scrollbar-win11 {
+        scrollbar-width: thin;
+        scrollbar-color: rgba(255,255,255,0.2) transparent;
+    }
+
+    .scrollbar-win11:active {
+        scrollbar-color: rgba(255,255,255,0.4) transparent;
+    }
+
+</style>
