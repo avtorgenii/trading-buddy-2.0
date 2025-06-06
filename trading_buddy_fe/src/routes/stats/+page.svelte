c6c6b9debf0e59d6066f0cc5b467ec000abe6939
Tomek4861 <tomek4861g@gmail.com>
2025-06-06 23:23:45 +0200

    feat: add stats page, add trade page

---
+++ trading_buddy_fe/src/routes/stats/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import PnLCalendar from '$lib/components/PnLCalendar.svelte';
+	import NavMenu from '$lib/components/NavMenu.svelte';
+
+
+
+	const myProfitLossData = {
+		'2025-06-05': -150.75,
+		'2025-06-09': 25.10,
+		'2025-06-18': 340.50,
+		'2025-06-20': -25.00,
+		'2025-06-21': 0.00,
+	};
+</script>
+
+
+<NavMenu />
+<div class="p-0">
+	<h1 class="text-2xl text-center font-bold mb-4">Trading Performance</h1>
+
+	<PnLCalendar data={myProfitLossData} />
+
+
+
+</div>
