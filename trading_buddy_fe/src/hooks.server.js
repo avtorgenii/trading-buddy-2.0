a19583e3a140622922fc9ac1346ed4b4ef37d73d
Aleksandr Shestakov <103767386+avtorgenii@users.noreply.github.com>
2025-08-06 00:12:02 +1000

    fix: image serving works, fixed couple of other issues

--- trading_buddy_fe/src/hooks.server.js
+++ trading_buddy_fe/src/hooks.server.js
@@ -7,28 +7,27 @@ export const handle = async ({ event, resolve }) => {
 	const sessionId = event.cookies.get('sessionid');
 	// get all cookies
 	// console.log("All cookies:", event.cookies.getAll());
-	console.log("Session ID from cookie:", sessionId);
+	// console.log("Session ID from cookie:", sessionId);
 
 	if (sessionId) {
-		try { //TODO; change the URL when fixed
+		try {
 			const response = await event.fetch(`${NON_PROXY_API_BASE_URL}/auth/status`, {
 				headers: {
 					Cookie: `sessionid=${sessionId}`
 				}
 			});
 
-			console.log("API response status:", response.status);
+			// console.log("API response status:", response.status);
 
 			if (response.ok) {
 				event.locals.user = await response.json();
-				console.log("User set in locals:", event.locals.user);
+				// console.log("User set in locals:", event.locals.user);
 			} else {
-				console.log("API returned non-ok status, clearing user.");
+				// console.log("API returned non-ok status, clearing user.");
 				event.locals.user = null;
 			}
-
 		} catch (error) {
-			console.error('API error in hooks.server.js:', error);
+			// console.error('API error in hooks.server.js:', error);
 			event.locals.user = null;
 		}
 	}
